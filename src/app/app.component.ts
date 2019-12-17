a659d21640703b51a92800251cc1041092fae001
anshumansworld <jaiswal.anshuman@tftus.com>
2019-12-17 16:03:50 +0530

    integrated github service.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { GitHubService } from "./service/github.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'github-poc';
+  commitList = [];
+  showSpinner: Boolean = false;
+  username: String = "anshumansworld";
+  repo: String = "github-example-app";
+
+  constructor(private gitHubService: GitHubService) {}
+
+  getRepoCommit() {
+    this.showSpinner = true;
+    this.gitHubService.getRepoCommit(this.username, this.repo).subscribe(
+      res => {
+        this.showSpinner = false;
+        this.commitList = res.body;
+      },
+      err => {
+        this.showSpinner = false;
+        this.commitList = []
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from './service/api.service';
 import { GitHubService } from './service/github.service';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule],
   providers: [
     ApiService,
